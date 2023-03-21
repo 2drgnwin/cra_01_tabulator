14b2b68dc673ea372d2c6e06a4cf0463dde8684b
YSL <2drgnwin@naver.com>
2023-03-21 23:32:54 +0900

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState, useEffect } from 'react';
+import 'react-tabulator/lib/styles.css';
+import 'react-tabulator/lib/css/tabulator.min.css'; // theme
+import { ReactTabulator } from 'react-tabulator';
+import { data, columns } from './data';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactTabulator data={data} columns={columns} layout={'fitColumns'} />
     </div>
   );
 }
